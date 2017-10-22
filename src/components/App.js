d6e82b3a333ae58954f8f2c45e8e55eb130f37ef
Michał Dudak <michal.dudak@gmail.com>
2017-10-22 13:45:31 +0200

    Moving things around

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import ProductList from './products/ProductList';
+import MainNavigation from './commons/MainNavigation';
 
 export default class App extends Component {
 
 	render() {
-		return <ProductList />
+		return (
+			<div className='page'>
+				<header>
+					<h1><a href="/">React Store</a></h1>
+					<MainNavigation />
+				</header>
+				<ProductList />
+			</div>
+		)
 	}
 }
---
+++ src/components/commons/MainNavigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function MainNavigation() {
+	return (
+		<div className="menu">
+			<a href="/">
+				<i className="fa fa-home" title="Home"></i>
+			</a>
+		</div>
+	);
+}
